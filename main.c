88cabfb4343bdf4192ae3575b5ffa40bafd25b16
iridiumR <i@iridium.cyou>
2022-09-18 11:24:31 +0800

    feat(main): add SIGINT detect

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
-#include <stdio.h>
 #include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static volatile int keepRunning = 1;
+
+void intHandler(int dummy) {
+  printf("\nend!\n");
+  keepRunning = 0;
+}
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World!");
-    #ifdef DEBUG
-    printf("DEBUG is defined");
-    #endif
-    return 0;
+
+  signal(SIGINT, intHandler);
+
+  printf("Hello, World!\n");
+#ifdef DEBUG
+  printf("DEBUG is defined\n");
+#endif
+  while (keepRunning) {
+    printf("runing!\n");
+    sleep(1);
+  }
+  return 0;
 }
